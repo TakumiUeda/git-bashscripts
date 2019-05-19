f96fd55cfd36be1b90b9c5704d0fa642404c1172
Takumi Ueda <reinforce00111@gmail.com>
2019-05-19 16:55:05 +0900

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -2,5 +2,8 @@
 # Auther:Takumi Ueda
 # This software is released under the MIT License, see LICENSE file.
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
 git pull
 read -p "Press [Enter] key to exit."
