028565636501df1a80a0adbdb1e8847bd1d33a6f
Takumi Ueda <reinforce00111@gmail.com>
2019-05-19 16:55:59 +0900

    Update push_orgin_master.sh

--- push_orgin_master.sh
+++ push_orgin_master.sh
@@ -2,6 +2,9 @@
 # Auther:Takumi Ueda
 # This software is released under the MIT License, see LICENSE file.
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
 git add .
 
 echo -n Please enter commit message:
